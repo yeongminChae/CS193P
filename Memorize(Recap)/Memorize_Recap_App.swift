8ca8cdca3a12473a3bf444e085afc368627251a1
Yeongmin_Chae <codudals98@naver.com>
2023-09-04 19:44:06 +0900

    Initial Commit

---
+++ Memorize(Recap)/Memorize_Recap_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Recap_App.swift
+//  Memorize(Recap)
+//
+//  Created by 채영민 on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Recap_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
