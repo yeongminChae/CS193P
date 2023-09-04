8d034ed6676841c3966d9992778947ba8947c472
Yeongmin_Chae <codudals98@naver.com>
2023-09-04 23:09:32 +0900

    Lecture 2

--- Memorize(Recap)/ContentView.swift
+++ Memorize(Recap)/ContentView.swift
@@ -7,33 +7,110 @@
 
 import SwiftUI
 
+// 기존 ContentView_Old 캡슐화한 코드로 리팩토링 진행
+
 struct ContentView: View {
+    let emojis = ["💀","🎃","👻","😈","🍭","🍬","🕯️","🦇","🙀","😱"]
+    @State var cardCount = 4
+
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjuster
+        }
+        .padding()
+    }
+
+    var cards: some View {
+//        return HStack {
+//        이것은 computed property로써 ViewBuilder가 아닙니다. 이와같이 코드가 한 줄만 있는 경우라면 return은 필요로 하지 않습니다. 이러한 구성을 Implicit return (암시적 반환)이라고 합니다.
+
+//        HStack {
+//        LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id:\.self) { emoji in
+                CardView(content: emojis[emoji])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundColor(.orange)
-        .padding() // 외부 주위에 패딩을 원할 경우, 크기가 커지기 시작할 것을 대비해 장치 가장자리에 부딪히기 않기를 원할 것입니다.
     }
+
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+//        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill") Implicit return
+    }
+
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+
+    //    Old var cardRemover: some View {
+    //    var cardRemover: some View {
+    //        Button(action: {
+    //            if cardCount > 1 {
+    //                cardCount -= 1
+    //            }
+    //        }, label: {
+    //            Image(systemName: "rectangle.stack.badge.minus.fill")
+    //        })
+    //    }
+
+
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp = true
 
     var body: some View {
+//        Bug : 이 기존 코드에서는 LazyVGrid를 사용하면 카드를 최대한 작게 만들 수 있고 이모지가 없으면 카드를 표시하는 카드가 정말 작을 수 있기 때문에 UI가 이상해 보이는 에러가 발생한다.
+//        ZStack {
+//            let base = RoundedRectangle(cornerRadius: 12)
+//            if isFaceUp {
+//                base.fill(.white)
+//                base.strokeBorder(lineWidth:  2)
+//                Text(content).font(.largeTitle)
+//            } else {
+//                base.fill()
+//            }
+//        }
+
+//      위와 같은 버그를 해결하기 위해 opacity를 사용한다. 이 메커니즘은 모든 상태에 대해 크기 조정과 간격이 필요한 두 상태 사이를 전환하는 좋은 방법이다.
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth:  2)
-                Text("👻").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group { // Group는 일종의 ForEach와 같다.
+                base.fill(.white)
+                base.strokeBorder(lineWidth:  2)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
---
+++ Memorize(Recap)/ContentView_Old.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  Memorize(Recap)
+//
+//  Created by 채영민 on 2023/09/04.
+//
+
+import SwiftUI
+
+struct ContentView_Old: View {
+    let emojis = ["💀","🎃","👻","😈","🍭","🍬","🕯️","🦇","🙀","😱"]
+
+    // Array is generic type
+    // 좋은 프로그래밍 기법은 상수/변수들을 사용하는 곳 근처에 선언하는 것이지만 이 배열은 최상단에 위치시켜 각각 뷰들이 접근하게끔 선언.
+    @State var cardCount = 4 // State는 실제 코드에서는 그렇게 많이 사용하지는 않지만 데모용으로는 좋습니다.
+    var body: some View {
+        VStack{
+            HStack {
+                ForEach(0..<cardCount, id:\.self) { emoji in
+                    //            ForEach(emojis.indices, id:\.self) { emoji in
+                    CardView(content: emojis[emoji])
+                }
+                //                Old Way
+                //                CardView(content: "💀",isFaceUp: false)
+                //                CardView(content: "🎃",isFaceUp: true)
+                //                CardView(content: "😈",isFaceUp: true)
+            }
+            .foregroundColor(.orange)
+
+            HStack {
+//                Button("Remove Card") {
+//                    cardCount -= 1
+//                }
+                Button(action: {
+                    if cardCount > 1 {
+                        cardCount -= 1
+                    }
+                }, label: {
+                    // 이 클로저는 ViewBuilder이다.
+                    Image(systemName: "rectangle.stack.badge.minus.fill")
+                })
+                Spacer()
+                Button(action: {
+                    if cardCount < emojis.count {
+                        cardCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.plus.fill")
+                })
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+                // 시스템 이미지는 근처에 있는 사물의 글꼴을 추적하려고 하는 것입니다. 그들은 인라인으로 작업하려고 시도하므로 어떤 글꼴로 설정하든 그에 맞춰 일치하게 되고 이미지 크기는 그에 비례하게 됩니다.
+
+        }
+        .padding()
+        // 이 VStack에 패딩을 적용한다면, foregroundColor와 같이 VStack 내부에 모든 것에 적용되지 않고,
+        // VStack 자체가 패딩되고 전체 주위에 패딩을 두는 것이 합리적입니다.
+    }
+}
+
+struct CardView_Old: View {
+//  old case =  var isFaceUp = true
+    let content: String
+    @State var isFaceUp = true
+
+    var body: some View {
+        ZStack {
+//            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12) // Type Inference
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth:  2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+
+            // old case
+            // isFaceUp.toggle()
+            // Error : Cannot use mutating member on immutable value: 'self' is immutable
+            // 이 self는 뷰 그 자체이고, 바뀔수 없습니다.
+            // 해결법 : @State var isFaceUp = true
+        }
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
